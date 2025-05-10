9216f1ecb38ecf6cb1ba71e749ad2ac4dde07c58
MacBook Air <macbookair@Air-MacBook.home>
2025-05-10 21:29:24 +0500

    Implement weather_functions module, next work with weather module.

--- backend/weather_functions.py
+++ backend/weather_functions.py
@@ -1,1 +1,16 @@
 import requests
+import streamlit as st
+
+API_KEY = st.secrets["API_KEY"]
+
+def get_data(place, days):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url=url)
+    data = response.json()
+
+    filtered_data = data['list']
+    values_amount = 8 * days 
+    filtered_data = filtered_data[:values_amount] # filtered_data[8] -> 1 day 
+
+    return filtered_data
+
--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from backend.weather_functions import get_data
 
 st.title('Weather Forecast')
 
@@ -7,3 +8,6 @@ days = st.slider('Forecast Days', min_value=1, max_value=5, help='Select the num
 option = st.selectbox('Select the type of weather to view', ('Temperature', 'Sky Conditions'))
 
 st.subheader(f"{option} for the next {days} days in {place}:")
+
+if place:
+    filtered_data = get_data(place, days)
